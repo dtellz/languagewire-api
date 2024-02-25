b99ad2940ef8dfd5e61de0c69fa167d65fbe4cb0
Diego <dtellez95@gmail.com>
2024-02-25 11:48:15 +0000

    setup cors middleware

--- server/main.py
+++ server/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from server.routers import translations
 from server.exceptions.handler import http_exception_handler
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
 
 # Custom exception handler
 app.add_exception_handler(HTTPException, http_exception_handler)
